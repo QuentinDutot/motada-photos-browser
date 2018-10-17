44074714f2071af388a0f79991f60b47c67b0856
Quentin Dutot <dutot.quentin@gmail.com>
2018-10-17 21:15:51 +0200

    Google analytics

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import { I18n } from 'react-i18nify';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
@@ -12,6 +13,11 @@ I18n.setTranslations(translations);
 I18n.setLocale(
   (localStorage.getItem('motada_language') || navigator.language || navigator.userLanguage || 'en').substring(0,2)
 );
+
+if(window.location.href.indexOf('localhost') === -1) {
+  ReactGA.initialize('UA-127688890-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 ReactDOM.render(
   <Provider store={createStore(reducer)}>
