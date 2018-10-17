9d71113ff794d068b38c539a615a641f330c1700
Quentin Dutot <dutot.quentin@gmail.com>
2018-10-17 23:51:26 +0200

    More europeans translations

--- src/assets/translations/translations.js
+++ src/assets/translations/translations.js
@@ -1,6 +1,10 @@
+import de from './de.json';
 import en from './en.json';
+import es from './es.json';
 import fr from './fr.json';
+import it from './it.json';
+import pt from './pt.json';
 
 export default {
-  en, fr,
+  de, en, es, fr, it, pt,
 };
