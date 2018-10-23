f6fe9ff10c830e156b5c665b026956023baaae66
QuentinDutot <dutot.quentin@gmail.com>
2018-10-23 14:42:13 +0200

    Fix pexels bot

--- src/server/scrapers/pexels.js
+++ src/server/scrapers/pexels.js
@@ -3,81 +3,91 @@ const chrome = require('selenium-webdriver/chrome');
 const randomWord = require('random-words');
 
 const rootPath = '/html/body/div[1]/div[2]/div[3]/article';
-const infosPath = '/a/img';
-
-const search = 'https://www.pexels.com/search';
 let driver;
 
-function scrapeItems(callback) {
-  const items = [];
-  driver.findElements(webdriver.By.xpath(rootPath)).then((elements) => {
-    console.log(`${elements.length} items loaded`);
-    console.log('Scraping data...');
+const buildDriver = () => new Promise((resolve, reject) => {
+  try {
+    const options = webdriver.Capabilities.chrome().set('chromeOptions', { 'args': ['--headless', '--window-size=400,650', '--no-sandbox'] });
+    driver = new webdriver.Builder().withCapabilities(options).build();
+    resolve('Driver built');
+  } catch(error) {
+    reject(`Can't build the driver : ${error}`);
+  }
+});
 
-    elements.forEach((row, rowIndex) => {
-      const customImage = {
-        url: '',
-        title: '',
-        source: 'pexels',
-        tags: [],
-      };
+const loadItems = async () => {
+  let items, newItems;
+  do {
+    // Load all items on the page
+    items = await driver.findElements(webdriver.By.xpath(rootPath));
+    console.log(`Items loaded : ${items.length}`);
 
-      row.findElement(webdriver.By.xpath(`${rootPath}[${rowIndex + 1}]${infosPath}`)).then((image) => {
-        image.getAttribute('alt').then((title) => { // image's title
-          customImage.title = title.indexOf('Free stock photo of ') !== -1 ? title.slice(20) : title;
-          customImage.title.split(' ').forEach(tags => { // image's tags
-            let tag = tags.split(',').join('');
-            tag = tag.charAt(0).toUpperCase()+tag.slice(1);
-            if(tag.length > 1
-              && tag !== 'Or' && tag !== 'And' && tag !== 'By'
-              && tag !== 'The' && tag !== 'In' && tag !== 'Of'
-              && tag !== 'Not' && tag !== 'To' && tag !== 'On') {
-              customImage.tags.push(tag);
-            }
-          });
-          image.getAttribute('srcset').then((url) => { // image's url
-            [customImage.url] = url.split('?');
-            items.push(customImage);
-            if (rowIndex === elements.length - 1) {
-              callback(items);
-            }
-          });
-        });
-      });
-    });
-  });
-}
+    // Try to load more by scrolling
+    await driver.executeScript('arguments[0].scrollIntoView(true);', items[items.length - 1])
+    await driver.sleep(1000 + Math.floor(Math.random() * 1000));
 
-function loadAllItems(callback) {
-  console.log('Loading items...');
-  driver.findElements(webdriver.By.xpath(rootPath)).then((elements) => {
-    driver.executeScript('arguments[0].scrollIntoView(true);', elements[elements.length - 1]);
-    driver.sleep(1000 + Math.floor(Math.random() * 1000)).then(() => {
-      driver.findElements(webdriver.By.xpath(rootPath)).then((newElements) => {
-        if (newElements.length > elements.length) {
-          loadAllItems(callback);
-        } else {
-          callback();
-        }
-      });
-    });
-  });
-}
+    // Test if there is new items
+    newItems = await driver.findElements(webdriver.By.xpath(rootPath));
+  } while(newItems.length > items.length);
+  return items;
+};
 
-module.exports = async (callback) => {
-  console.log('pexels scraper started');
-  driver = new webdriver.Builder()
-            .withCapabilities(webdriver.Capabilities.chrome().set('chromeOptions', { 'args': ['--headless', '--window-size=400,650', '--no-sandbox'] }))
-            .build();
-  const url = `${search}/${randomWord()}`;
-  console.log(url);
-  driver.get(url).then(() => {
-    loadAllItems(() => {
-      scrapeItems((items) => {
-        driver.quit();
-        console.log(items);
-        callback(items);
-      });
-    });
-  });
+const scrapeItems = async (items) => {
+  const images = [];
+  let itemIndex = 0;
+  while(itemIndex < items.length) {
+    // Get wanted element
+    const image = await driver.findElement(webdriver.By.xpath(`${rootPath}[${itemIndex + 1}]/a/img`));
+
+    // Building custom object
+    const customImage = { source: 'pexels', tags: [], };
+    customImage.title = await image.getAttribute('alt');
+    customImage.title = customImage.title.indexOf('Free stock photo of ') !== -1 ? customImage.title.slice(20) : customImage.title;
+    [customImage.url] = (await image.getAttribute('srcset')).split('?');
+
+    // Adding tags
+    const tags = customImage.title.split(' ');
+    for(let tag of tags) {
+      tag = tag.split(',').join('');
+      tag = tag.split('.').join('');
+      tag = tag.charAt(0).toUpperCase()+tag.slice(1);
+      if(tag.length > 1
+        && tag !== 'Or' && tag !== 'And' && tag !== 'By'
+        && tag !== 'The' && tag !== 'In' && tag !== 'Of'
+        && tag !== 'Not' && tag !== 'To' && tag !== 'On') {
+        customImage.tags.push(tag);
+      }
+    }
+
+    // Store our custom object
+    images.push(customImage);
+    itemIndex++;
+  }
+  return images;
+};
+
+module.exports = async (persist) => {
+  console.log('Pexels scraper ON');
+  const url = `https://www.pexels.com/search/${randomWord()}`;
+
+  try {
+    // Initializations
+    await buildDriver()
+    console.log('Driver built');
+    await driver.get(url);
+    console.log(url);
+
+    // Engine
+    const items = await loadItems();
+    const images = await scrapeItems(items);
+    await driver.quit();
+
+    // Finish stuff
+    persist(images);
+    console.log(images);
+    console.log('Pexels scraper OFF : success');
+
+  } catch(error) {
+    console.log(`Pexels scraper OFF : ${JSON.stringify(error)}`);
+  }
 };
